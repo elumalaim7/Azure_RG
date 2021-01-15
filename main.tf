373dd0ad7da17b357a4113c9f4a6df064b2e2195
Elumalai <48083867+elumalaim7@users.noreply.github.com>
2021-01-15 23:26:05 +0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "<=2.0.0"
+
+
+  subscription_id = var.subscriptionID
+  client_id       = var.clinetID
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantID
+
+
+  features {}
+
+}
+
+resource "azurerm_resource_group" "dev-rg" {
+  name     = var.RGName
+  location = var.location
+}
